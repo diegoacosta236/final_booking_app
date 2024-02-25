4556f879f285491490fbd33d8bfa4ff583afd3b2
diegoacosta236 <diego.acosta.cohort236@gmail.com>
2024-02-25 13:40:49 -0800

    finished backend

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
@@ -9,16 +9,26 @@ router.get("/checkauth", verifyToken, (req, res, next) => {
     res.send("Logged in.")
 })
 
+// Verify a user
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("You are logged in and able to delete your account.")
+})
+
+// Verify that a user is an Admin
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+    res.send("You are logged in and able to delete whatever you want.")
+})
+
 // Update
-router.put('/:id', updateUser)
+router.put('/:id', verifyUser,updateUser)
 
 // Delete
-router.delete('/:id', deleteUser)
+router.delete('/:id', verifyUser,deleteUser)
 
 // Get
-router.get('/:id', getUser)
+router.get('/:id', verifyUser,getUser)
 
 // Get All
-router.get('/', getUsers)
+router.get('/', verifyAdmin,getUsers)
 
 export default router
